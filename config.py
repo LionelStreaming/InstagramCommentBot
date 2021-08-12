06d1a6d1958c14fc80f21cbaa833b4f4332ded13
LionelStreaming <84461615+LionelStreaming@users.noreply.github.com>
2021-08-12 16:26:01 -0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,20 @@
+# Enter username and password of instagram account: 
+username = 'awdadwadwadwawdadwdw'
+password = 'awdadwadwadwawdadw'
+
+# Delay time between comments (seconds):
+delay = 5
+
+# Amount of comments for each tag or location:
+amount = 10
+
+# Commenting by tags - 'tag'. Commenting by locations - 'location'
+type_commenting = 'tag'
+# type_commenting = 'location'
+
+tags = ['fortnite', 'minecraft', 'roblox', 'pubg', 'cod', 'csgo']
+
+locations = ['204517928/chicago-illinois', '234901722/oral-kazakhstan']
+
+# Comments (the script will take a random):
+comments = ['ᶦᵐ ᶠᵒˡˡᵒʷᶦⁿᵍ ᵗᵒ ᵉᵛᵉʳʸᵒⁿᵉ ʷʰᵒ ᶠᵒˡˡᵒʷˢ ᵐᵉ ᵃˢʷᵉˡˡ', 'ᶦᵐ ᶠᵒˡˡᵒʷᶦⁿᵍ ᵗᵒ ᵉᵛᵉʳʸᵒⁿᵉ ʷʰᵒ ᶠᵒˡˡᵒʷˢ ᵐᵉ ᵃˢʷᵉˡˡ', 'ᶦᵐ ᶠᵒˡˡᵒʷᶦⁿᵍ ᵗᵒ ᵉᵛᵉʳʸᵒⁿᵉ ʷʰᵒ ᶠᵒˡˡᵒʷˢ ᵐᵉ ᵃˢʷᵉˡˡ']
